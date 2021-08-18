9c41ce239b6d893a91e9f82409198a04627bc66c
Amank-root <70597017+Amank-root@users.noreply.github.com>
2021-08-18 17:52:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+import Content from './Components/Content';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Content />
+    </>
+      );
+}
+
+      export default App;
