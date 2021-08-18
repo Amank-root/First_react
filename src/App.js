3782be4c3f67d972e4dbef01219149200c173b83
Amank-root <amank.root@gmail.com>
2021-08-18 22:28:40 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import React from 'react'
-
 import Content from './Components/Content';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar title="React" />
       <Content />
     </>
       );
