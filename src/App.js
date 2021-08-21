b90a620c8c8bda3ea54a3aebdac39440dabf238f
Amank-root <amank.root@gmail.com>
2021-08-21 17:45:19 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './Components/Hero';
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,44 +1,26 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap';
 
 function Content() {
     return (
         <>
-            <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img
-                        className="d-block w-100"
-                        src="https://fantasy-expert.netlify.app/assets/blog/t.png"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src="https://fantasy-expert.netlify.app/assets/blog/s.png"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://fantasy-expert.netlify.app/assets/blog/b1b.png"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            <section className="hero">
+                <div className="container flow">
+                    <div className="hero__content flow">
+                        <h1 className="section-title">Welcome to ReactDev</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ipsum.<q><b>Edits</b></q>.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat consectetur aspernatur, exercitationem nihil sed tempora maxime ullam officia fugit ipsam.</p>
+                        <p>Lorem<q><b>Lorem</b></q> Lorem, ipsum dolor.</p>
+                        <div className="flex-group">
+                            <a href="#featured-articles" className="btn btn--primary">Lorem, ipsum dolor.</a>
+                            <a href="./blog" className="btn btn--neutral">Lorem, ipsum dolor.</a>
+                        </div>
+                    </div>
+                    <div>
+
+                        <img src="//unsplash.it/510" alt="" className="hero__image" />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
